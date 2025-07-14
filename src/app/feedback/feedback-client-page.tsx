331e0b34e3b96db49fd262d36031e4b24c6f1615
REYAZXSID <reyazmbi2003@gmail.com>
2025-07-14 16:05:04 +0530

    Update feedback-client-page.tsx

--- src/app/feedback/feedback-client-page.tsx
+++ src/app/feedback/feedback-client-page.tsx
@@ -108,7 +108,7 @@ export function FeedbackClientPage() {
             </CardHeader>
             <CardContent>
                  <Button asChild className="w-full">
-                    <Link href="https://www.instagram.com/reyaz_sid" target="_blank" rel="noopener noreferrer">
+                    <Link href="https://www.instagram.com/sidverse.0" target="_blank" rel="noopener noreferrer">
                         <Instagram className="mr-2 h-5 w-5" />
                         Contact Us!
                         <ArrowRight className="ml-auto h-5 w-5" />
